fde9f57d0de01b58c6e84c836d61f6c338361f10
Ankit Mishra <ankit618731@gmail.com>
2019-09-07 06:11:16 +0530

    UI + backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+var session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mysql      = require('mysql');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = process.env.PORT || 3000
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -36,32 +44,113 @@ app.get('/', (req,res) =>{
           res.render('login');
 });
 
+app.post('/', (req,res) =>{
+  let {username, password} = req.body;
+  console.log(username,password);
+  if (username && password) {
+    const sql = "SELECT * FROM admin WHERE username = '"+ username +"' AND password = '"+ password +"'";
+    connection.query(sql, function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+        req.session.username = username;
+        res.redirect('/dashboard');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+    });
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
+
 app.get('/dashboard', (req,res) =>{
-  res.render('dashboard');
+      isloggedin(req,res,'dashboard');
 });
 
 app.get('/add-quiz', (req,res) =>{
-  res.render('add-quiz');
+  isloggedin(req,res,'add-quiz');
+});
+
+app.get('/add-current-affairs', (req,res) =>{
+  isloggedin(req,res,'add-current-affairs');
 });
 
 app.get('/take-quiz', (req,res) =>{
-  const sql = 'SELECT * FROM questions';
+  res.render('take-quiz');
+});
+
+isloggedin = (req,res,page) => {
+  if (req.session.loggedin) {
+    res.render(page);
+} else {
+  res.redirect('/');
+}
+};
+
+app.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+app.get('/take-subject-quiz', (req,res) =>{
+  const sql = 'SELECT * FROM subject_quiz ORDER BY qid DESC LIMIT 5 ';
   connection.query(sql, function (error, results, fields) {
       if (error){
           throw error;
       }else{
-        let answerkey = new Array();;
+        let answerkey = new Array();
+        let answer1 = new Array();
+        let answer2 = new Array();
+        let answer3 = new Array();
+        let answer4 = new Array();
         for(let i = 0; i < results.length; i++){
-          answerkey[i] = results[i].answer 
+          answerkey[i] = results[i].answer;
+          answer1[i] = results[i].optiona;
+          answer2[i] = results[i].optionb;
+          answer3[i] = results[i].optionc;
+          answer4[i] = results[i].optiond; 
         }
-        res.render('take-quiz',{data: results,answers: answerkey});
+        res.render('take-subject-quiz',{data: results,answers: answerkey,optiona: answer1,optionb: answer2,optionc: answer3,optiond: answer4});
+      }    
+  });
+});
+
+app.get('/current-affairs-quiz', (req,res) =>{
+  const sql = 'SELECT * FROM current_affair_quiz ORDER BY qid DESC LIMIT 5 ';
+  connection.query(sql, function (error, results, fields) {
+      if (error){
+          throw error;
+      }else{
+        let answerkey = new Array();
+        let answer1 = new Array();
+        let answer2 = new Array();
+        let answer3 = new Array();
+        let answer4 = new Array();
+        for(let i = 0; i < results.length; i++){
+          answerkey[i] = results[i].answer;
+          answer1[i] = results[i].optiona;
+          answer2[i] = results[i].optionb;
+          answer3[i] = results[i].optionc;
+          answer4[i] = results[i].optiond; 
+        }
+        res.render('current-affairs-quiz',{data: results,answers: answerkey,optiona: answer1,optionb: answer2,optionc: answer3,optiond: answer4});
       }    
   });
 });
 
 app.post('/add-quiz', (req,res) =>{
   const {question, optionA, optionB, optionC, optionD, answer} = req.body;
-  const sql = "INSERT INTO questions (question, optiona, optionb, optionc, optiond, answer) VALUES ('"+ question +"', '"+ optionA +"', '"+ optionB +"','"+ optionC +"','"+ optionD +"','"+ answer +"')";
+  const sql = "INSERT INTO subject_quiz (question, optiona, optionb, optionc, optiond, answer) VALUES ('"+ question +"', '"+ optionA +"', '"+ optionB +"','"+ optionC +"','"+ optionD +"','"+ answer +"')";
   connection.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
@@ -69,12 +158,14 @@ app.post('/add-quiz', (req,res) =>{
   });
 });
 
-app.get('/add-current-affairs', (req,res) =>{
-  res.render('add-current-affairs');
-});
-
-app.post('/', (req,res) =>{
-  res.redirect('/dashboard');
+app.post('/add-current-affairs', (req,res) =>{
+  const {question, optionA, optionB, optionC, optionD, answer} = req.body;
+  const sql = "INSERT INTO current_affair_quiz (question, optiona, optionb, optionc, optiond, answer) VALUES ('"+ question +"', '"+ optionA +"', '"+ optionB +"','"+ optionC +"','"+ optionD +"','"+ answer +"')";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    res.redirect('/add-quiz');
+  });
 });
 
 const server = app.listen(port, (req, res) => {
