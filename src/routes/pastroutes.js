64ae3536e3aa370ff848128af3b8b9a233b5e9c0
Ankit Mishra <ankit618731@gmail.com>
2019-09-14 01:11:24 +0530

    past questions module updated

--- src/routes/pastroutes.js
+++ src/routes/pastroutes.js
@@ -19,8 +19,6 @@ router.get('/', (req,res) =>{
             array[i] = results[i].Date.toDateString();
             array[i] = array[i].split(",");
           }
-          // var demodate = results[0].Date.toDateString();
-          // demodate = demodate.split(" ");
           res.render('past-questions',{output: array,title:'Past Static Syllabus Quizzes',id:'1'});
         }    
     });
@@ -39,8 +37,6 @@ router.get('/current-affairs-quiz', (req,res) =>{
             array[i] = results[i].Date.toDateString();
             array[i] = array[i].split(",");
           }
-          // var demodate = results[0].Date.toDateString();
-          // demodate = demodate.split(" ");
           res.render('past-questions',{output: array,title:'Past Current Affair Quizzes',id:'2'});
         }    
     });
@@ -58,8 +54,6 @@ router.get('/take-subjective-quiz', (req,res) =>{
             array[i] = results[i].Date.toDateString();
             array[i] = array[i].split(",");
           }
-          // var demodate = results[0].Date.toDateString();
-          // demodate = demodate.split(" ");
           res.render('past-questions',{output: array,title:'Past GS Mains Questions',id:'3'});
         }    
     });
@@ -77,8 +71,6 @@ router.get('/current-affairs-subjective-quiz', (req,res) =>{
             array[i] = results[i].Date.toDateString();
             array[i] = array[i].split(",");
           }
-          // var demodate = results[0].Date.toDateString();
-          // demodate = demodate.split(" ");
           res.render('past-questions',{output: array,title:'Past Current Affairs',id:'4'});
         }    
     });
@@ -86,7 +78,7 @@ router.get('/current-affairs-subjective-quiz', (req,res) =>{
   
 
 
-router.get('/past/', (req,res) =>{
+router.get('/past-subject/', (req,res) =>{
       
       function getMonthFromString(mon){
         return new Date(Date.parse(mon +" 11, 1111")).getMonth()+1
@@ -94,9 +86,8 @@ router.get('/past/', (req,res) =>{
       let month = getMonthFromString(req.query.month);
       let date = req.query.date;
       let year = req.query.year;
-      console.log(date);
       
-      const sql = 'SELECT * FROM current_affair_quiz WHERE MONTH(Date) = ? AND YEAR(Date) = ? AND DAY(Date) = ?';
+      const sql = 'SELECT * FROM subject_quiz WHERE MONTH(Date) = ? AND YEAR(Date) = ? AND DAY(Date) = ?';
       connection.query(sql,[month, year, date], function (error, results, fields) {
         if (error){
             throw error;
@@ -115,9 +106,80 @@ router.get('/past/', (req,res) =>{
             answer4[i] = results[i].optiond;
             questions[i] = results[i].question;
           }
-          res.render('current-affairs-quiz',{answers: answerkey,optiona: answer1,optionb: answer2,optionc: answer3,optiond: answer4,question:questions});
+          res.render('take-subject-quiz',{answers: answerkey,optiona: answer1,optionb: answer2,optionc: answer3,optiond: answer4,question:questions});
         }    
     });
   });
+
+router.get('/past-current-affairs/', (req,res) =>{
+      
+    function getMonthFromString(mon){
+      return new Date(Date.parse(mon +" 11, 1111")).getMonth()+1
+    }
+    let month = getMonthFromString(req.query.month);
+    let date = req.query.date;
+    let year = req.query.year;
+    
+    const sql = 'SELECT * FROM current_affair_quiz WHERE MONTH(Date) = ? AND YEAR(Date) = ? AND DAY(Date) = ?';
+    connection.query(sql,[month, year, date], function (error, results, fields) {
+      if (error){
+          throw error;
+      }else{
+        let answerkey = new Array();
+        let questions = new Array();
+        let answer1 = new Array();
+        let answer2 = new Array();
+        let answer3 = new Array();
+        let answer4 = new Array();
+        for(let i = 0; i < results.length; i++){
+          answerkey[i] = results[i].answer;
+          answer1[i] = results[i].optiona;
+          answer2[i] = results[i].optionb;
+          answer3[i] = results[i].optionc;
+          answer4[i] = results[i].optiond;
+          questions[i] = results[i].question;
+        }
+        res.render('current-affairs-quiz',{answers: answerkey,optiona: answer1,optionb: answer2,optionc: answer3,optiond: answer4,question:questions});
+      }    
+  });
+});
+
+router.get('/past-GS-subjective/', (req,res) =>{
+      
+  function getMonthFromString(mon){
+    return new Date(Date.parse(mon +" 11, 1111")).getMonth()+1
+  }
+  let month = getMonthFromString(req.query.month);
+  let date = req.query.date;
+  let year = req.query.year;
   
+  const sql = 'SELECT * FROM  quiz_subjective WHERE MONTH(Date) = ? AND YEAR(Date) = ? AND DAY(Date) = ?';
+  connection.query(sql,[month, year, date], function (error, results, fields) {
+    if (error){
+        throw error;
+    }else{
+      res.render('quiz-subjective',{data:results});
+    }    
+  });
+});
+
+router.get('/past-current-affairs-subjective/', (req,res) =>{
+      
+  function getMonthFromString(mon){
+    return new Date(Date.parse(mon +" 11, 1111")).getMonth()+1
+  }
+  let month = getMonthFromString(req.query.month);
+  let date = req.query.date;
+  let year = req.query.year;
+  
+  const sql = 'SELECT * FROM current_affair_subjective WHERE MONTH(Date) = ? AND YEAR(Date) = ? AND DAY(Date) = ?';
+  connection.query(sql,[month, year, date], function (error, results, fields) {
+    if (error){
+        throw error;
+    }else{
+      res.render('current-affairs-subjective',{data:results});
+    }    
+  });
+});
+
 module.exports = router;     
